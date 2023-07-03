9241c9c330362b851f7553f50e2cc062c308b825
ale2589 <133082542+ale2589@users.noreply.github.com>
2023-07-02 21:10:52 -0500

    Modificación footer
    Se agregó el ícono, nombres de integrantes y el ícono de home

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -1,22 +1,47 @@
 import React from 'react';
 import { styled } from '@mui/material/styles';
-import { Typography, Container } from '@mui/material';
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import HomeIcon from '@mui/icons-material/Home';
+import { Typography, Box, Button } from '@mui/material';
+
+
+
 
 const FooterContainer = styled('footer')(({ theme }) => ({
   padding: theme.spacing(2),
   marginTop: 'auto',
-  backgroundColor: theme.palette.primary.main,
+  backgroundColor: "#07090A",
   color: theme.palette.primary.contrastText,
 }));
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <Container maxWidth="sm">
-        <Typography variant="body1">
-          Este es el contenido de mi footer.
-        </Typography>
-      </Container>
+      <Box display="flex" flexDirection="row" justifyContent="space-between">
+       <Box>
+        <Typography variant="body1"
+         sx={{      
+           color: "#FFF",
+           marginLeft: "5px",
+           fontWeight: "bold",
+         }}
+         >
+         <HealthAndSafetyIcon sx={{ fontSize: "17px", color: "#FE9A22" }} /> 
+        SECURITY CELLS
+         </Typography>
+      </Box>
+      <Box>
+      <Typography sx={{
+           fontSize: "10px",   
+           color: "#FFF",
+           marginLeft: "5px",
+         }} 
+      >Integrantes: Yannick De La Torre, Joel del Hierrro, Alejandra Escobar.</Typography>
+      </Box>
+      <Box>
+      <Button href='/'><HomeIcon sx={{ fontSize: "15px", color: "#FFF" }} /></Button> 
+      </Box>
+      </Box>
     </FooterContainer>
   );
 };
